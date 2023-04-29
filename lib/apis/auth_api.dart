aa735a3a10c7fa6803216199c248f19efd17f43d
Kamakshi Karakoti <kamakshikarakoti22@gmail.com>
2023-04-29 21:13:43 +0530

    update

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,16 +1,36 @@
+import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:twitter_clone/core/core.dart';
+import 'package:twitter_clone/core/providers.dart';
+
+final authAuthAPIProvider = Provider((ref){
+  return AuthAPI(account: ref.watch(appwriteAccountProvider));
+});
 
 abstract class IAuthAPI {
   FutureEither<User> signUp({required String email, required String password});
 }
 
+/* -------------------------------------------------------------------------- */
+/*                  create user account in appwrite dashboard                 */
+/* -------------------------------------------------------------------------- */
 class AuthAPI implements IAuthAPI {
-  final User _account;
-  AuthAPI({required User account}) : _account = account;
+  final Account _account;
+  AuthAPI({required Account account}) : _account = account;
   @override
-  FutureEither<User> signUp({required String email, required String password}) {
-    // TODO: implement signUp
-    throw UnimplementedError();
+  FutureEither<User> signUp(
+      {required String email, required String password}) async {
+    try {
+      final account = await _account.create(
+          userId: ID.unique(), email: email, password: password);
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+          Failure(e.message ?? 'Some unexpected error occurred', stackTrace));
+    } catch (e, stackTrace) {
+      return left(Failure(e.toString(), stackTrace));
+    }
   }
 }
